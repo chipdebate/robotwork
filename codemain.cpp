70158f814e5779622f2e35df97c21d3d350bd90c
Christoph Wilkie <167291977+chipdebate@users.noreply.github.com>
2024-04-17 10:49:27 +0300

    Create codemain.cpp

---
+++ codemain.cpp
@@ -0,0 +1,29 @@
+ void Init()
+  {    
+    case_sensitive_ = false;
+    current = nullptr; end = nullptr; last_found = nullptr;
+  }
+
+  bool FindNext_CaseSensitive()
+  {
+    while (current < end)
+    {
+      uint8_t i = shift[static_cast<unsigned>(*current)];  //compare last byte first
+      if (!i)                   //last byte matches if i == 0
+      {
+        char* j = current - needle_len_less1;
+        while (i < needle_len_less1 && needle_[i] == *(j + i)) ++i;
+        if (i == needle_len_less1)
+        {
+          ++current;
+          last_found = j;
+          return true;
+        }
+        else
+          current += jump_;
+      }
+      else
+        current += i;
+    }
+    return false;
+  }
